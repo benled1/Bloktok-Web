dc765731165a8a755452def1f4a5a2001b88608b
Ben Ledingham <benledingham1@gmail.com>
2024-05-01 14:34:11 -0700

    added source videos and completing the video generator

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from video_generator import video_generator
 from video_generator.reddit_parser.reddit_auth import *
+from typing import Dict
 
 def main():
-    video_generator.get_audio("https://www.reddit.com/r/AmItheAsshole/comments/1chp01s/aita_for_refusing_to_make_a_cookie_table_for_my/")
+    audio_info: Dict = video_generator.get_audio("https://www.reddit.com/r/AmItheAsshole/comments/1chp01s/aita_for_refusing_to_make_a_cookie_table_for_my/")
+    video_generator.get_video(text_body=audio_info["text_body"], audio_file=audio_info["audio_filepath"])
     pass
 
 if __name__ == "__main__":
--- src/video_generator/video_generator.py
+++ src/video_generator/video_generator.py
@@ -1,11 +1,11 @@
 import os
+import moviepy.editor as mpe
 from .reddit_parser import reddit
 from .audio_generator import tts_generator
 from typing import Dict
 from dotenv import load_dotenv
 
 load_dotenv()
-AUDIO_FILEPATH = os.getenv("TMP_AUDIO_PATH") 
 
 def get_audio(url: str) -> Dict:
     post_id = reddit.get_post_id(url=url)
@@ -14,6 +14,20 @@ def get_audio(url: str) -> Dict:
     post_body = reddit.get_post_body(reddit_data=post_data)
 
     content = post_title + "\n" + post_body
-    file_path = tts_generator.write_text_to_speech(content, post_id, AUDIO_FILEPATH) # this makes an API call to OPENAI
+    file_path = tts_generator.write_text_to_speech(content, post_id, os.getenv("TMP_AUDIO_PATH")) # this makes an API call to OPENAI
 
     return {"text_body": content, "audio_filepath": file_path}
+
+
+def get_video(text_body: str, audio_file: str):
+
+    audio_bg = mpe.AudioFileClip(audio_file)
+
+    audio_duration = audio_bg.duration
+    videos = []
+    for i in range(0,4):
+        videos.append(mpe.VideoFileClip(f"{os.getenv('SOURCE_VIDEOS_PATH')}/minecraft_parkour_{i}.mp4")) # add a video path env variable
+    v_clip = mpe.concatenate_videoclips(videos)
+    print(f"VIDEO_DURATION = {v_clip.duration}")
+    print(f"AUDIO DURATION = {audio_duration}")
+    pass
