b5cad0506a513cbe6db2f38318e8252ddca2c473
Ben Ledingham <benledingham1@gmail.com>
2024-05-04 18:53:41 -0700

    works in the docker container, just need to install imagemagik

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,11 @@ ENV PYTHONBUFFERED 1
 
 WORKDIR /app
 COPY . /app
-
+ENV DOTENV_PATH="/app/.env"
 RUN apt-get update && \
       apt-get -y install sudo && \
-      apt-get install -y libyaml-dev
+      apt-get install -y libyaml-dev && \
+      apt-get install -y vim
 RUN pip install -r /app/requirements.txt
 
-ENTRYPOINT ["/bin/bash"]
+ENTRYPOINT ["python", "src/main.py"]
--- src/video_generator/reddit_parser/reddit_auth.py
+++ src/video_generator/reddit_parser/reddit_auth.py
@@ -10,7 +10,7 @@ def refresh_bearer_token() -> None:
     post_data = {"grant_type": "password", "username": os.getenv("REDDIT_USERNAME"), "password": os.getenv("REDDIT_PASSWORD")}
     headers = {"User-Agent": "ChangeMeClient/0.1"}
     response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-    set_key("/home/benled/dev/Bloktok-Web/.env", "REDDIT_BEARER_TOKEN", response.json()["access_token"])
+    set_key(os.environ.get("DOTENV_PATH"), "REDDIT_BEARER_TOKEN", response.json()["access_token"])
 
 def get_access_token() -> str:
    return os.getenv("REDDIT_BEARER_TOKEN") 
