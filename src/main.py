e93efebc07462eaf54cab876744f9d89eec39376
Ben Ledingham <benledingham1@gmail.com>
2024-05-07 19:21:46 -0700

    added fastapi but port forwarding doesn't work(think it's due to docker in wsl)

--- src/main.py
+++ src/main.py
@@ -2,12 +2,18 @@ from video_generator import video_generator
 from video_generator.reddit_parser.reddit_auth import *
 from typing import Dict
 from dotenv import load_dotenv
+from fastapi import FastAPI
 
-def main():
-    load_dotenv()
+app = FastAPI()
+load_dotenv()
+
+def run_video_generator():
     audio_info: Dict = video_generator.get_audio("https://www.reddit.com/r/AmItheAsshole/comments/1chp01s/aita_for_refusing_to_make_a_cookie_table_for_my/")
     video_generator.get_video(text_body=audio_info["text_body"], audio_file=audio_info["audio_filepath"])
-    pass
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    # run_video_generator()
+    return {"message": "SUCESS!"}
+
+
